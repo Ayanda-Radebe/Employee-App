7d8eace33564602e60ac75a02a5a57ac98e6d824
Ayanda-Radebe <radebeayanda11@gmail.com>
2024-08-23 14:27:02 +0200

    ReadMe added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 // src/App.js
-import React, { useState } from 'react';
-import LoginPage from './components/LoginPage';
-import EmployeePortal from './components/EmployeePortal';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import LoginPage from "./components/LoginPage";
+import EmployeePortal from "./components/EmployeePortal";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    return (
-        <div className="App">
-            {isLoggedIn ? (
-                <EmployeePortal />
-            ) : (
-                <LoginPage onLogin={setIsLoggedIn} />
-            )}
-        </div>
-    );
+  return (
+    <div className="App">
+      {isLoggedIn ? <EmployeePortal /> : <LoginPage onLogin={setIsLoggedIn} />}
+    </div>
+  );
 }
 
 export default App;
